8ca5f166e421669d2365946b104f16b9952c1f51
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-04-02 16:18:24 -0400

    Add 'tidy()' and 'get_and_create_deckpath()' functions to architecture sketch.

--- architecture.py
+++ architecture.py
@@ -1,7 +1,11 @@
 """Architecture for a cleaner version of _clone()."""
-def clone():
+from typing import Dict, List
+from pathlib import Path
+
+
+def _clone():
     paths: Dict[int, Path] = {}
-    decks: Dict[str, KiNote] = {}
+    decks: Dict[str, List[KiNote]] = {}
     for nid in collection:
         kinote = KiNote(nid)
         decks[kinote.deck] = decks.get(kinote.deck, []) + [kinote]
@@ -9,9 +13,9 @@ def clone():
             if has_html(field):
                 paths[nid] = dump(field)
     tidy(paths)
-    for deck in sorted(set(decks.keys())):
-        deckpath = get_and_create_deckpath(deck)
-        for kinote in decks[deck]:
+    for deckname in sorted(set(decks.keys()), key=len, reverse=True):
+        deckpath = get_and_create_deckpath(deckname)
+        for kinote in decks[deckname]:
             notepath = get_notepath(kinote, deckpath)
             payload = get_tidy_payload(kinote, paths)
             notepath.write_text(payload, encoding="UTF-8")
@@ -19,3 +23,14 @@ def clone():
 
 def dump(field: str) -> Path:
     pass
+
+def tidy(paths: Dict[int, Path]) -> None:
+    pass
+
+def get_and_create_deckpath(deckname: str) -> Path:
+    # Strip leading periods so we don't get hidden folders.
+    components = deckname.split("::")
+    components = [re.sub(r"^\.", r"", comp) for comp in components]
+    deckpath = Path(targetdir, *components)
+    deckpath.mkdir(parents=True, exist_ok=True)
+    return deckpath
