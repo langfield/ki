435cbef98fed4f64c06e80e1a454052259be0d8c
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-04-04 13:41:28 -0400

    Delete 'ki/architecture.py'.
    Move files from 'architecture.py' into '__init__.py'.

--- ki/__init__.py
+++ ki/__init__.py
@@ -953,3 +953,75 @@ def slugify(value: str, allow_unicode: bool = False) -> str:
         )
     value = re.sub(r"[^\w\s-]", "", value.lower())
     return re.sub(r"[-\s]+", "-", value).strip("-_")
+
+
+@beartype
+def write_notes(colpath: Path, targetdir: Path, silent: bool):
+    """Write notes to appropriate directories in ``targetdir``."""
+    # Create temp directory for htmlfield text files.
+    root = Path(tempfile.mkdtemp()) / "ki" / "fieldhtml"
+    root.mkdir(parents=True, exist_ok=True)
+
+    paths: Dict[str, Path] = {}
+    decks: Dict[str, List[KiNote]] = {}
+
+    # Open deck with `apy`, and dump notes and markdown files.
+    with Anki(path=colpath) as a:
+        all_nids = list(a.col.find_notes(query=""))
+        for nid in tqdm(all_nids, ncols=TQDM_NUM_COLS, disable=silent):
+            kinote = KiNote(a, a.col.get_note(nid))
+            decks[kinote.deck] = decks.get(kinote.deck, []) + [kinote]
+            for fieldname, fieldtext in kinote.fields.items():
+                if re.search(HTML_REGEX, fieldtext):
+                    fid = get_field_note_id(nid, fieldname)
+                    paths[fid] = root / fid
+                    paths[fid].write_text(fieldtext)
+
+        tidy_html_recursively(root, silent)
+        for deckname in sorted(set(decks.keys()), key=len, reverse=True):
+            deckpath = create_deckpath(deckname, targetdir)
+            for kinote in decks[deckname]:
+                sort_fieldname = get_sort_fieldname(a, kinote.n.note_type())
+                notepath = get_notepath(kinote, sort_fieldname, deckpath)
+                payload = get_tidy_payload(kinote, paths)
+                notepath.write_text(payload, encoding="UTF-8")
+
+
+@beartype
+def create_deckpath(deckname: str, targetdir: Path) -> Path:
+    """Construct path to deck directory and create it."""
+    # Strip leading periods so we don't get hidden folders.
+    components = deckname.split("::")
+    components = [re.sub(r"^\.", r"", comp) for comp in components]
+    deckpath = Path(targetdir, *components)
+    deckpath.mkdir(parents=True, exist_ok=True)
+    return deckpath
+
+
+@beartype
+def get_field_note_id(nid: int, fieldname: str) -> str:
+    """A str ID that uniquely identifies field-note pairs."""
+    return f"{nid}{slugify(fieldname, allow_unicode=True)}"
+
+
+@beartype
+def get_tidy_payload(kinote: KiNote, paths: Dict[str, Path]) -> str:
+    """Get the payload for the note (HTML-tidied if necessary)."""
+    # Get tidied html if it exists.
+    tidyfields = {}
+    for fieldname, fieldtext in kinote.fields.items():
+        fid = get_field_note_id(kinote.n.id, fieldname)
+        if fid in paths:
+            tidyfields[fieldname] = paths[fid].read_text()
+        else:
+            tidyfields[fieldname] = fieldtext
+
+    # Construct note repr from tidyfields map.
+    lines = kinote.get_header_lines()
+    for fieldname, fieldtext in tidyfields.items():
+        lines.append("### " + fieldname)
+        lines.append(fieldtext)
+        lines.append("")
+
+    # Dump payload to filesystem.
+    return "\n".join(lines)
