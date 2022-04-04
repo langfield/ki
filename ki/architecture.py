89a1bdb5980b2499c59d76ae9eda02d5f9a7ee3e
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-04-04 13:10:45 -0400

    Remove 'dump_field()', 'tidy()' from architecture.py.

--- ki/architecture.py
+++ ki/architecture.py
@@ -1,16 +1,14 @@
 """Architecture for a cleaner version of _clone()."""
 import re
 import tempfile
-import subprocess
 from typing import Dict, List
 from pathlib import Path
 
 from tqdm import tqdm
 from apy.anki import Anki
 
-from ki import KiNote, get_sort_fieldname, get_notepath, get_batches, slugify
+from ki import KiNote, get_sort_fieldname, get_notepath, slugify, tidy_html_recursively
 
-BATCH_SIZE = 500
 HTML_REGEX = r"</?\s*[a-z-][^>]*\s*>|(\&(?:[\w\d]+|#\d+|#x[a-f\d]+);)"
 TQDM_NUM_COLS = 70
 
@@ -33,8 +31,10 @@ def _write_notes(colpath: Path, targetdir: Path, silent: bool):
             for fieldname, fieldtext in kinote.fields.items():
                 if re.search(HTML_REGEX, fieldtext):
                     fid = get_field_note_id(nid, fieldname)
-                    paths[fid] = dump_field(fid, fieldtext, root)
-        tidy(paths, silent)
+                    paths[fid] = root / fid
+                    paths[fid].write_text(fieldtext)
+
+        tidy_html_recursively(root, silent)
         for deckname in sorted(set(decks.keys()), key=len, reverse=True):
             deckpath = get_and_create_deckpath(deckname, targetdir)
             for kinote in decks[deckname]:
@@ -42,23 +42,6 @@ def _write_notes(colpath: Path, targetdir: Path, silent: bool):
                 notepath = get_notepath(kinote, sort_fieldname, deckpath)
                 payload = get_tidy_payload(kinote, paths)
                 notepath.write_text(payload, encoding="UTF-8")
-
-
-def dump_field(fid: str, fieldtext: str, root: Path) -> Path:
-    """Dump field with given fid to a unique path."""
-    htmlpath = root / fid
-    htmlpath.write_text(fieldtext)
-    return htmlpath
-
-
-def tidy(paths: Dict[str, Path], silent: bool) -> None:
-    """Spin up subprocesses for tidying field HTML in-place."""
-    path_strings = [str(path) for path in paths.values()]
-    batches = list(get_batches(path_strings, BATCH_SIZE))
-    for batch in tqdm(batches, ncols=TQDM_NUM_COLS, disable=silent):
-        pathsline = " ".join(batch)
-        command = f"tidy -q -m -i -omit -utf8 --tidy-mark no {pathsline}"
-        subprocess.run(command, shell=True, check=False, capture_output=True)
 
 
 def get_and_create_deckpath(deckname: str, targetdir: Path) -> Path:
