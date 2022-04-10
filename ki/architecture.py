aa1ad400b8622d17cf5248be54eca61f7ea847d8
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-04-10 08:33:07 -0400

    Add docstring to 'architecture.py'.

--- ki/architecture.py
+++ ki/architecture.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 """
-Push architecture redesign.
+Python package `ki` is a command-line interface for the version control and
+editing of ``.anki2`` collections as git repositories of markdown files.
+Rather than providing an interactive UI like the Anki desktop client, `ki` aims
+to allow natural editing *in the filesystem*.
+
+In general, the purpose of `ki` is to allow users to work on large, complex
+Anki decks in exactly the same way they work on large, complex software
+projects.
+.. include:: ./DOCUMENTATION.md
 """
 
-# pylint: disable=invalid-name, unused-import, missing-class-docstring, broad-except
+# pylint: disable=invalid-name, missing-class-docstring, broad-except
 # pylint: disable=too-many-return-statements, too-many-lines
 
 import os
