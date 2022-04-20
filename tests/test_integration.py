3561b623cdfc6b797ced14c5958ab4be5b63c87a
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-04-20 10:43:06 -0400

    Fix broken imports in 'tests/test_integration.py'.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -24,8 +24,8 @@ from beartype.typing import List
 
 import ki
 import ki.functional as F
-from ki import (
-    BRANCH_NAME,
+from ki import BRANCH_NAME, get_colnote
+from ki.types import (
     ColNote,
     ExtantDir,
     ExtantFile,
@@ -33,8 +33,6 @@ from ki import (
     TargetExistsError,
     NotKiRepoError,
     UpdatesRejectedError,
-    get_colnote,
-    is_anki_note,
 )
 
 
