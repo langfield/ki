8851f64485e277edd926cbded398bf063f9e75fb
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-07-08 04:00:02 -0400

    Refactor unit tests `get_media_files()` -> `copy_media_files()`.
    Add missing media target directory argument in tests for above function.

--- tests/test_ki.py
+++ tests/test_ki.py
@@ -76,7 +76,7 @@ from ki import (
     push_decknote_to_anki,
     get_models_recursively,
     append_md5sum,
-    get_media_files,
+    copy_media_files,
     diff2,
     _clone,
 )
@@ -1556,8 +1556,8 @@ def test_fmkleaves_handles_collisions(tmp_path):
         assert len(os.listdir(".")) == 0
 
 
-def test_get_media_files_returns_nice_errors():
-    """Does `get_media_files()` handle case where media directory doesn't exist?"""
+def test_copy_media_files_returns_nice_errors():
+    """Does `copy_media_files()` handle case where media directory doesn't exist?"""
     col_file: ExtantFile = get_media_col_file()
     col: Collection = open_collection(col_file)
     runner = CliRunner()
@@ -1568,7 +1568,7 @@ def test_get_media_files_returns_nice_errors():
         shutil.rmtree(media_dir)
 
         with pytest.raises(MissingMediaDirectoryError) as error:
-            get_media_files(col, silent=True)
+            copy_media_files(col, F.mkdtemp(), silent=True)
         assert "media.media" in str(error.exconly())
         assert "bad Anki collection media directory" in str(error.exconly())
 
@@ -1602,14 +1602,14 @@ def test_write_repository_displays_missing_media_warnings(capfd):
         assert "media.media/1sec.mp3" in captured.out
 
 
-def test_get_media_files_finds_notetype_media():
-    """Does `get_media_files()` get files like `collection.media/_vonNeumann.jpg`?"""
+def test_copy_media_files_finds_notetype_media():
+    """Does `copy_media_files()` get files like `collection.media/_vonNeumann.jpg`?"""
     col_file: ExtantFile = get_media_col_file()
     col: Collection = open_collection(col_file)
     runner = CliRunner()
     with runner.isolated_filesystem():
 
-        media, warnings = get_media_files(col, silent=True)
+        media, warnings = copy_media_files(col, F.mkdtemp(), silent=True)
         media_files: Set[ExtantFile] = set()
         for media_set in media.values():
             media_files = media_files | media_set
