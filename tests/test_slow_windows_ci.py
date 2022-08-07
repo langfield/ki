5fcfb3f20933b1f567e57836a27f38eb202b44dc
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-08-07 22:44:15 +0200

    Add `tests/test_slow_windows_ci.py`.

---
+++ tests/test_slow_windows_ci.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Tests for ki command line interface (CLI)."""
+import os
+from importlib.metadata import version
+
+from beartype import beartype
+from beartype.typing import List
+
+import ki
+from tests.test_ki import invoke
+
+
+# CLI
+
+import subprocess
+
+
+@beartype
+def capture(args: List[str]) -> subprocess.CompletedProcess:
+    return subprocess.run(
+        args,
+        text=True,
+        capture_output=True,
+    )
+
+
+def test_version():
+    """Does --version display information as expected?"""
+    expected_version = version("ki")
+    result = invoke(ki.ki, ["--version"])
+
+    assert result.stdout == f"ki, version {expected_version}{os.linesep}"
+    assert result.exit_code == 0
+
+
+def test_version_clickless():
+    """Does --version display information as expected?"""
+    expected_version = version("ki")
+    p = capture(["ki", "--version"])
+
+    assert p.stdout == f"ki, version {expected_version}{os.linesep}"
+    assert p.returncode == 0
