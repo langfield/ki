455a2fc5cb1eb17894cd33c974e1063b67dd5d21
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-08-08 16:56:40 +0200

    Use gitpython's `rmtree()`.

--- ki/functional.py
+++ ki/functional.py
@@ -61,27 +61,7 @@ SLUG_REGEX = re.compile(r"[^\w\s\-" + EMOJIS + PICTOGRAPHS + TRANSPORTS + FLAGS 
 @beartype
 def rmtree(target: ExtantDir) -> NoFile:
     """Equivalent to `shutil.rmtree()`, but annihilates read-only files on Windows."""
-
-    # TODO: Type hint this handler.
-    def onerror(func, path, exc_info):
-        """
-        Error handler for ``shutil.rmtree``.
-
-        If the error is due to an access error (read only file)
-        it attempts to add write permission and then retries.
-
-        If the error is for another reason it re-raises the error.
-
-        Usage : ``shutil.rmtree(path, onerror=onerror)``
-        """
-        # Is the error an access error?
-        if not os.access(path, os.W_OK):
-            os.chmod(path, stat.S_IWUSR)
-            func(path)
-        else:
-            raise
-
-    shutil.rmtree(target, onerror=onerror)
+    git.rmtree(target)
     return NoFile(target)
 
 
