758c7a6429c8f74867d8770fd45041c7077aff60
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-11-12 17:25:06 -0500

    Make sql parser tests more strict.

--- tests/test_sql_parser.py
+++ tests/test_sql_parser.py
@@ -1,8 +1,11 @@
 """Tests for SQLite Lark grammar."""
 
+from pathlib import Path
 import pytest
 import prettyprinter as pp
-from ki.sqlite import SQLiteTransformer
+import ki
+from ki.types import SQLNote
+from ki.sqlite import SQLiteTransformer, Table, Insert
 from tests.test_parser import get_parser, debug_lark_error
 
 
@@ -28,7 +31,7 @@ def test_transformer():
     parser = get_parser(filename="sqlite.lark", start="diff")
     tree = parser.parse(BLOCK)
     transformer = SQLiteTransformer()
-    _ = transformer.transform(tree)
+    out = transformer.transform(tree)
 
 
 UPDATE = r"""
@@ -180,13 +183,13 @@ def test_transformer_on_decimals_in_sort_field():
         raise err
     transformer = SQLiteTransformer()
     out = transformer.transform(tree)
-    pp.pprint(out)
+    assert len(out) == 1
+    x = out[0]
+    assert x == Insert(table=Table.Notes, data=SQLNote(mid=1667061149792, guid='q6qBOHAT=6', tags=(), flds=(".1", '')))
 
 
-COL_UPDATE = r"""
-UPDATE col SET mod=1651363200000, conf='{"curDeck":1,"sortBackwards":false,"collapseTime":1200,"curModel":1667061149792,"estTimes":true,"_nt_1667061149792_lastDeck":1,"activeDecks":[1],"nextPos":2,"dueCounts":true,"timeLim":0,"sortType":"noteFld","dayLearnFirst":false,"newSpread":0,"addToCur":true,"_deck_1_lastNotetype":1667061149792,"schedVer":2,"creationOffset":240}', models='{"1667061149793":{"id":1667061149793,"name":"Basic (and reversed card)","type":0,"mod":0,"usn":0,"sortf":0,"did":null,"tmpls":[{"name":"Card 1","ord":0,"qfmt":"{{Front}}","afmt":"{{FrontSide}}\n\n<hr id=answer>\n\n{{Back}}","bqfmt":"","bafmt":"","did":null,"bfont":"","bsize":0},{"name":"Card 2","ord":1,"qfmt":"{{Back}}","afmt":"{{FrontSide}}\n\n<hr id=answer>\n\n{{Front}}","bqfmt":"","bafmt":"","did":null,"bfont":"","bsize":0}],"flds":[{"name":"Front","ord":0,"sticky":false,"rtl":false,"font":"Arial","size":20,"description":""},{"name":"Back","ord":1,"sticky":false,"rtl":false,"font":"Arial","size":20,"description":""}],"css":".card {\n    font-family: arial;\n    font-size: 20px;\n    text-align: center;\n    color: black;\n    background-color: white;\n}\n","latexPre":"\\documentclass[12pt]{article}\n\\special{papersize=3in,5in}\n\\usepackage[utf8]{inputenc}\n\\usepackage{amssymb,amsmath}\n\\pagestyle{empty}\n\\setlength{\\parindent}{0in}\n\\begin{document}\n","latexPost":"\\end{document}","latexsvg":false,"req":[[0,"any",[0]],[1,"any",[1]]]},"1667061149796":{"id":1667061149796,"name":"Cloze","type":1,"mod":0,"usn":0,"sortf":0,"did":null,"tmpls":[{"name":"Cloze","ord":0,"qfmt":"{{cloze:Text}}","afmt":"{{cloze:Text}}<br>\n{{Back Extra}}","bqfmt":"","bafmt":"","did":null,"bfont":"","bsize":0}],"flds":[{"name":"Text","ord":0,"sticky":false,"rtl":false,"font":"Arial","size":20,"description":""},{"name":"Back Extra","ord":1,"sticky":false,"rtl":false,"font":"Arial","size":20,"description":""}],"css":".card {\n    font-family: arial;\n    font-size: 20px;\n    text-align: center;\n    color: black;\n    background-color: white;\n}\n\n.cloze {\n font-weight: bold;\n color: blue;\n}\n.nightMode .cloze {\n color: lightblue;\n}\n","latexPre":"\\documentclass[12pt]{article}\n\\special{papersize=3in,5in}\n\\usepackage[utf8]{inputenc}\n\\usepackage{amssymb,amsmath}\n\\pagestyle{empty}\n\\setlength{\\parindent}{0in}\n\\begin{document}\n","latexPost":"\\end{document}","latexsvg":false,"req":[[0,"any",[0]]]},"1667061149795":{"id":1667061149795,"name":"Basic (type in the answer)","type":0,"mod":0,"usn":0,"sortf":0,"did":null,"tmpls":[{"name":"Card 1","ord":0,"qfmt":"{{Front}}\n\n{{type:Back}}","afmt":"{{Front}}\n\n<hr id=answer>\n\n{{type:Back}}","bqfmt":"","bafmt":"","did":null,"bfont":"","bsize":0}],"flds":[{"name":"Front","ord":0,"sticky":false,"rtl":false,"font":"Arial","size":20,"description":""},{"name":"Back","ord":1,"sticky":false,"rtl":false,"font":"Arial","size":20,"description":""}],"css":".card {\n    font-family: arial;\n    font-size: 20px;\n    text-align: center;\n    color: black;\n    background-color: white;\n}\n","latexPre":"\\documentclass[12pt]{article}\n\\special{papersize=3in,5in}\n\\usepackage[utf8]{inputenc}\n\\usepackage{amssymb,amsmath}\n\\pagestyle{empty}\n\\setlength{\\parindent}{0in}\n\\begin{document}\n","latexPost":"\\end{document}","latexsvg":false,"req":[[0,"any",[0,1]]]},"1667061149794":{"id":1667061149794,"name":"Basic (optional reversed card)","type":0,"mod":0,"usn":0,"sortf":0,"did":null,"tmpls":[{"name":"Card 1","ord":0,"qfmt":"{{Front}}","afmt":"{{FrontSide}}\n\n<hr id=answer>\n\n{{Back}}","bqfmt":"","bafmt":"","did":null,"bfont":"","bsize":0},{"name":"Card 2","ord":1,"qfmt":"{{#Add Reverse}}{{Back}}{{/Add Reverse}}","afmt":"{{FrontSide}}\n\n<hr id=answer>\n\n{{Front}}","bqfmt":"","bafmt":"","did":null,"bfont":"","bsize":0}],"flds":[{"name":"Front","ord":0,"sticky":false,"rtl":false,"font":"Arial","size":20,"description":""},{"name":"Back","ord":1,"sticky":false,"rtl":false,"font":"Arial","size":20,"description":""},{"name":"Add Reverse","ord":2,"sticky":false,"rtl":false,"font":"Arial","size":20,"description":""}],"css":".card {\n    font-family: arial;\n    font-size: 20px;\n    text-align: center;\n    color: black;\n    background-color: white;\n}\n","latexPre":"\\documentclass[12pt]{article}\n\\special{papersize=3in,5in}\n\\usepackage[utf8]{inputenc}\n\\usepackage{amssymb,amsmath}\n\\pagestyle{empty}\n\\setlength{\\parindent}{0in}\n\\begin{document}\n","latexPost":"\\end{document}","latexsvg":false,"req":[[0,"any",[0]],[1,"all",[1,2]]]},"1667061149792":{"id":1667061149792,"name":"Basic","type":0,"mod":0,"usn":0,"sortf":0,"did":null,"tmpls":[{"name":"Card 1","ord":0,"qfmt":"{{Front}}","afmt":"{{FrontSide}}\n\n<hr id=answer>\n\n{{Back}}","bqfmt":"","bafmt":"","did":null,"bfont":"","bsize":0}],"flds":[{"name":"Front","ord":0,"sticky":false,"rtl":false,"font":"Arial","size":20,"description":""},{"name":"Back","ord":1,"sticky":false,"rtl":false,"font":"Arial","size":20,"description":""}],"css":".card {\n    font-family: arial;\n    font-size: 20px;\n    text-align: center;\n    color: black;\n    background-color: white;\n}\n","latexPre":"\\documentclass[12pt]{article}\n\\special{papersize=3in,5in}\n\\usepackage[utf8]{inputenc}\n\\usepackage{amssymb,amsmath}\n\\pagestyle{empty}\n\\setlength{\\parindent}{0in}\n\\begin{document}\n","latexPost":"\\end{document}","latexsvg":false,"req":[[0,"any",[0]]]}}' WHERE id=1;
-"""
-
+with open(Path(ki.__file__).parent.parent / "tests" / "update.sql", "r", encoding="UTF-8") as f:
+    COL_UPDATE = f.read()
 
 def test_transformer_on_col_update_queries():
     parser = get_parser(filename="sqlite.lark", start="diff")
@@ -196,5 +199,4 @@ def test_transformer_on_col_update_queries():
         debug_lark_error(err)
         raise err
     transformer = SQLiteTransformer()
-    out = transformer.transform(tree)
-    pp.pprint(out)
+    transformer.transform(tree)
