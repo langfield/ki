fafb195739937f7fb923474e7cdbca4cd6688114
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-04-03 20:36:41 -0400

    Add first passing test!

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+import Path (Abs, Dir, Path)
 import Test.LeanCheck
 import Test.LeanCheck.Instances ()
 import Test.Hspec (describe, hspec, it, shouldBe)
@@ -11,4 +12,6 @@ main :: IO ()
 main = hspec $ do
   describe "getDir" $ do
     it "Does something reasonable when given an empty string" $ do
-      getDir "" `shouldBe` Path.Internal.Path ""
+      getDir "" `shouldBe` (Path.Internal.Path "./" :: Path a Dir)
+    it "Path.Internal.Path is well-defined" $ do
+      Path.Internal.Path "" `shouldBe` (Path.Internal.Path "" :: Path Abs Dir)
