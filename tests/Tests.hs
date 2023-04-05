a116ced87671ac2eae1ec62fca6daf4354ccbf40
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-04-04 20:53:24 -0400

    Add another test for `getDir`

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,17 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Path (Abs, Dir, Path)
-import Test.LeanCheck
+import Path (Dir, Path)
+import Test.LeanCheck ()
 import Test.LeanCheck.Instances ()
-import Test.Hspec (describe, hspec, it, shouldBe)
+import Test.Hspec (describe, hspec, it, shouldBe, shouldNotBe)
 
 import qualified Path.Internal
 
-import Ki (maxFilenameSize, getDir)
+import Ki (getDir)
 
 main :: IO ()
 main = hspec $ do
   describe "getDir" $ do
     it "Does something reasonable when given an empty string" $ do
       getDir "" `shouldBe` (Path.Internal.Path "./" :: Path a Dir)
-    it "Path.Internal.Path is well-defined" $ do
-      Path.Internal.Path "" `shouldBe` (Path.Internal.Path "" :: Path Abs Dir)
+    it "Encodes empty string as './', not an empty string (Is this the behavior we want?)" $ do
+      getDir "" `shouldNotBe` (Path.Internal.Path "" :: Path a Dir)
