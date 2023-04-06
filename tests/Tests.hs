dfbd57538a61cb7c6f4c7cabb45479f593d0a8c6
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-04-05 20:48:50 -0400

    Add two tests for handling of abs paths by `getDir`

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Path (Dir, Path)
+import Path (Abs, Dir, Path, toFilePath)
 import Test.LeanCheck ()
 import Test.LeanCheck.Instances ()
 import Test.Hspec (describe, hspec, it, shouldBe, shouldNotBe)
@@ -15,3 +15,8 @@ main = hspec $ do
       getDir "" `shouldBe` (Path.Internal.Path "./" :: Path a Dir)
     it "Encodes empty string as './', not an empty string (Is this the behavior we want?)" $ do
       getDir "" `shouldNotBe` (Path.Internal.Path "" :: Path a Dir)
+    it "Writes the correct data for absolute paths" $ do
+      getDir "a" `shouldBe` (Path.Internal.Path "/a" :: Path Abs Dir)
+    it "Gives a correct FilePath for absolute paths" $ do
+      toFilePath (getDir "a") `shouldBe` "/a"
+
