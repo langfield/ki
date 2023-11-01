05f5eaad7b106fa7dfc7f84efc88606e7ae624c2
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-10-31 22:40:28 -0400

    Make `subtree.sh` a user-facing script for creating subdecks

--- subtree.sh
+++ subtree.sh
@@ -1,31 +1,22 @@
 #!/usr/bin/env bash
-mkdir big-repo
-cd big-repo
-git init --initial-branch=main
+set -e
+subdeck=$(echo "$1" | sed 's,/*$,,')
+remote=$2
+root=$(git rev-parse --show-toplevel)
 
-echo ''
-echo '0' > 0
-git add .
-git commit -m "Initial commit"
+echo $root
 
-echo ''
-mkdir little-repo
-cd little-repo
-echo 'a' > a
-git add .
-git commit -m "little-repo"
-cd ../..
+rm -rf /tmp/ki
+mkdir -p /tmp/ki
+cd /tmp/ki
+git clone $root $subdeck
+cd $subdeck
+git filter-repo --force --subdirectory-filter $subdeck --path-rename $subdeck/:
+cd $(git rev-parse --show-toplevel)
+git remote add origin $remote
+git push -u origin main
 
-# Here we want to add stuff that will push this to github.
-# We will take the SSH remote as a command-line argument.
-echo ''
-git clone ./big-repo little-repo
-cd little-repo
-git filter-repo --force --subdirectory-filter little-repo --path-rename little-repo/:
-cd ..
-
-echo ''
-cd big-repo
-git rm -r little-repo
-git commit -m "Remove little-repo"
-git subtree add --prefix little-repo git@github.com:langfield/subtree.git main --squash
+cd $root
+git rm -r $subdeck
+git commit -m "Remove `$subdeck`"
+git subtree add --prefix $subdeck $remote main --squash
