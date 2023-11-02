e882d83fdbb2d7e20f1531a12b5d89cc5ea52f4c
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-11-01 22:19:58 -0400

    Add `target` argument to `subtree.sh`

--- subtree.sh
+++ subtree.sh
@@ -2,13 +2,14 @@
 set -e
 subdeck=$(echo "$1" | sed 's,/*$,,')
 remote=$2
+target=$3
 root=$(git rev-parse --show-toplevel)
 
-echo $root
+[ -d $target ] && echo "target directory '$target' already exists" && exit 1
+[ -f $target ] && echo "a file already exists at '$target'" && exit 1
 
-rm -rf /tmp/ki
-mkdir -p /tmp/ki
-cd /tmp/ki
+mkdir -p $target
+cd $target
 git clone $root $subdeck
 cd $subdeck
 git filter-repo --force --subdirectory-filter $subdeck --path-rename $subdeck/:
@@ -18,5 +19,6 @@ git push -u origin main
 
 cd $root
 git rm -r $subdeck
-git commit -m "Remove `$subdeck`"
+git commit -m "Remove \`$subdeck\`"
 git subtree add --prefix $subdeck $remote main --squash
+echo "repo for deck '$subdeck' created at '$target'"
